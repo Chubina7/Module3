6f42e8b2c683e44642f8f5f33ab772b60f0e5a53
Luka Chubinidze <chubinidzel08@gmail.com>
2023-11-30 15:55:58 +0400

    add changes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import styles from './page.module.css'
+import React from "react";
+import styles from "./page.module.css";
 
 export default function About() {
   return (
     <main className={styles.main}>
       <h1>ეს არის შესახებ</h1>
     </main>
-  )
+  );
 }
