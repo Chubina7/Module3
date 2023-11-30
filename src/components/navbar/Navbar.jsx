b979d18bd3e75eedbe66dda3e2fd2c1f24601687
Luka Chubinidze <chubinidzel08@gmail.com>
2023-11-30 17:38:04 +0400

    make user ui details (main page + about)

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import styles from './navbar.module.css'
-import Link from 'next/link'
+import React from "react";
+import styles from "./navbar.module.css";
+import Link from "next/link";
 
 export default function Navbar() {
   const links = [
-    { id: 1, title: 'Home', url: '/' },
-    { id: 2, title: 'Portfolio', url: '/portfolio' },
-    { id: 4, title: 'Blog', url: '/blog' },
-    { id: 5, title: 'About', url: '/about' },
-    { id: 6, title: 'Contact', url: '/contact' },
-    { id: 6, title: 'Dashboard', url: '/dashboard' },
-  ]
+    { id: 1, title: "Home", url: "/" },
+    { id: 2, title: "Portfolio", url: "/portfolio" },
+    { id: 4, title: "Blog", url: "/blog" },
+    { id: 5, title: "About", url: "/about" },
+    { id: 6, title: "Contact", url: "/contact" },
+    { id: 6, title: "Dashboard", url: "/dashboard" },
+  ];
 
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>LOGO</div>
+      <Link href='/' className={styles.logo}>Lamamia</Link>
       <div className={styles.links}>
-        {links.map(link => <Link href={link.url} className={styles.eachLink}>{link.title}</Link>)}
+        {links.map((link) => (
+          <Link href={link.url} className={styles.eachLink}>
+            {link.title}
+          </Link>
+        ))}
+        <button className={styles.logOutBtn}>Log Out</button>
       </div>
     </div>
-  )
+  );
 }
