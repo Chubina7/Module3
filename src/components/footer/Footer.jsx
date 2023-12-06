efcca40ef9ff9cbce6bb5444196f334e3f45d6c8
Luka Chubinidze <chubinidzel08@gmail.com>
2023-12-06 10:56:42 +0400

    add links on social icons\ in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,17 +1,50 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from './footer.module.css'
+import React from "react";
+import Image from "next/image";
+import styles from "./footer.module.css";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
       <span>@ All rights deserved</span>
       <div className={styles.socialNetworks}>
-        <Image src={'/1.png'} width={20} height={20} alt={'facebooks'}  className={styles.icon} />
-        <Image src={'/2.png'} width={20} height={20} alt={'instagram'} className={styles.icon} />
-        <Image src={'/3.png'} width={20} height={20} alt={'x'} className={styles.icon} />
-        <Image src={'/4.png'} width={20} height={20} alt={'youtube'} className={styles.icon} />
+        <Link href="https://www.facebook.com" target="_blank">
+          <Image
+            src={"/1.png"}
+            width={20}
+            height={20}
+            alt={"facebooks"}
+            className={styles.icon}
+          />
+        </Link>
+        <Link href="https://www.instagram.com" target="_blank">
+          <Image
+            src={"/2.png"}
+            width={20}
+            height={20}
+            alt={"instagram"}
+            className={styles.icon}
+          />
+        </Link>
+        <Link href="https://www.twitter.com" target="_blank">
+          <Image
+            src={"/3.png"}
+            width={20}
+            height={20}
+            alt={"x"}
+            className={styles.icon}
+          />
+        </Link>
+        <Link href="https://www.youtube.com" target="_blank">
+          <Image
+            src={"/4.png"}
+            width={20}
+            height={20}
+            alt={"youtube"}
+            className={styles.icon}
+          />
+        </Link>
       </div>
     </div>
-  )
+  );
 }
