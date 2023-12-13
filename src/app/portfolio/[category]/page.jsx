13259027b43ebca25b30271cab08b20d8bd3a9bb
Luka Chubinidze <chubinidzel08@gmail.com>
2023-12-13 17:03:13 +0400

    add portfolio category page

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -27,8 +27,108 @@ export default function Category({ params }) {
               "https://images.pexels.com/photos/19287812/pexels-photo-19287812/free-photo-of-black-and-white-photo-of-a-cow-laying-down.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             }
             alt="top"
-            width={10}
-            height={10}
+            width={100}
+            height={100}
+            className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <h1 className={styles.heading}>Test</h1>
+          <p className={styles.desc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad,
+            doloribus? Eveniet similique voluptatem culpa delectus nobis
+            molestiae quam ipsa quos! Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Quos laudantium nobis ratione unde, fugit autem
+            consequatur sequi eligendi reiciendis inventore nihil rerum sed
+            harum numquam deserunt obcaecati dolore exercitationem? Temporibus.
+          </p>
+          <Button title="See More" url={"/"} />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src={
+              "https://images.pexels.com/photos/19287812/pexels-photo-19287812/free-photo-of-black-and-white-photo-of-a-cow-laying-down.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            }
+            alt="top"
+            width={100}
+            height={100}
+            className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <h1 className={styles.heading}>Test</h1>
+          <p className={styles.desc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad,
+            doloribus? Eveniet similique voluptatem culpa delectus nobis
+            molestiae quam ipsa quos! Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Quos laudantium nobis ratione unde, fugit autem
+            consequatur sequi eligendi reiciendis inventore nihil rerum sed
+            harum numquam deserunt obcaecati dolore exercitationem? Temporibus.
+          </p>
+          <Button title="See More" url={"/"} />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src={
+              "https://images.pexels.com/photos/19287812/pexels-photo-19287812/free-photo-of-black-and-white-photo-of-a-cow-laying-down.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            }
+            alt="top"
+            width={100}
+            height={100}
+            className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <h1 className={styles.heading}>Test</h1>
+          <p className={styles.desc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad,
+            doloribus? Eveniet similique voluptatem culpa delectus nobis
+            molestiae quam ipsa quos! Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Quos laudantium nobis ratione unde, fugit autem
+            consequatur sequi eligendi reiciendis inventore nihil rerum sed
+            harum numquam deserunt obcaecati dolore exercitationem? Temporibus.
+          </p>
+          <Button title="See More" url={"/"} />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src={
+              "https://images.pexels.com/photos/19287812/pexels-photo-19287812/free-photo-of-black-and-white-photo-of-a-cow-laying-down.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            }
+            alt="top"
+            width={100}
+            height={100}
+            className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <h1 className={styles.heading}>Test</h1>
+          <p className={styles.desc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad,
+            doloribus? Eveniet similique voluptatem culpa delectus nobis
+            molestiae quam ipsa quos! Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Quos laudantium nobis ratione unde, fugit autem
+            consequatur sequi eligendi reiciendis inventore nihil rerum sed
+            harum numquam deserunt obcaecati dolore exercitationem? Temporibus.
+          </p>
+          <Button title="See More" url={"/"} />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src={
+              "https://images.pexels.com/photos/19287812/pexels-photo-19287812/free-photo-of-black-and-white-photo-of-a-cow-laying-down.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            }
+            alt="top"
+            width={100}
+            height={100}
             className={styles.img}
           />
         </div>
